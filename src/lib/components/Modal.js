ac9eaea4dd23139a8ec04452e8997c1e47767c6c
Delastef <delastef@hotmail.comgit config --global user.name delastefegit config --global user.email delastef@hotmail.com>
2023-07-22 01:48:19 +0200

    ajout de la documentation et publication sur npm

--- src/lib/components/Modal.js
+++ src/lib/components/Modal.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from "react"
 import "./modal.css"
 
+/**
+ *
+ * @param {string} active this is the class of the container for the modal
+ * @param {string} setActive this will set the class of the div to show or hide the modal. To show the modal set "active" and to hide it set ""
+ * @param {string} text it allows you to put the text that you want in the modal
+ * @returns {jsx} the modal
+ */
 const Modal = ({ active, setActive, text }) => {
   return (
-    <div className={`modalWrapper ${active ? "active": ""} `}>
+    <div className={`modalWrapper ${active ? "active" : ""} `}>
       <p>
         {text}
         <span onClick={() => setActive("")}>X</span>
       </p>
     </div>
-  );
+  )
 }
 export default Modal
